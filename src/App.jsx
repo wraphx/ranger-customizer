dc2731af817a45939b531a3c1b1b797b56c17611
Ty Byers <tyrone@wraphx.com>
2024-09-03 11:52:26 -0400

    Updated Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Suspense, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber"
-import { ContactShadows, Environment, Lightformer, OrbitControls, Loader } from "@react-three/drei"
+import { Suspense,  } from "react";
+import { Canvas,  } from "@react-three/fiber"
+import { ContactShadows, Environment, OrbitControls, Loader } from "@react-three/drei"
 
 import { CustomizationProvider } from "./context/Customization"
 import Configurator from "./components/Configurator";
@@ -13,25 +13,25 @@ export default function App() {
     <CustomizationProvider>
       <div className="App">
       <Loader />
-        <Canvas shadows camera={{ position: [0, 0, 20], fov:40 }}>
-          <color attach="background" args={['grey']} />
+        <Canvas  camera={{ position: [0, 0, 20], fov:40 }}>
+          
           <Suspense fallback={null}>
           <Ranger scale={6} position={[0, 0, 0]} />
           </Suspense>
-          <hemisphereLight intensity={0.5} />
+          <hemisphereLight intensity={2.5} />
           <ContactShadows position={[0, 0, 0.0]} opacity={0.75} scale={15} blur={2.5} far={0.8} />
           <ContactShadows resolution={1024} frames={1} position={[0, 0, 0]} scale={19} blur={0.5} opacity={0.5} far={20} />
           
-          <ambientLight intensity={0.7} />
-          <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+          <ambientLight intensity={3.7} />
+          
 
           {/* <Environment preset="city" /> */}
-          <Environment resolution={1024} files="/future_parking_2k.hdr" background ground={{ height: 15, radius: 80, scale: 50 }} backgroundBlurriness={50} backgroundIntensity={10}  environmentIntensity={30}>
+          <Environment resolution={1024} files="/future_parking_2k.hdr" background ground={{ height: 15, radius: 80, scale: 50 }} backgroundBlurriness={50} backgroundIntensity={40}  environmentIntensity={40}>
            
           </Environment>
           
           {/* <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={true} enablePan={false} /> */}
-          <OrbitControls  autoRotate  enablePan={false} enableZoom={true} minPolarAngle={Math.PI / 2.8} maxPolarAngle={Math.PI / 2.1} />
+          <OrbitControls    enablePan={false} enableZoom={true} minPolarAngle={Math.PI / 2.8} maxPolarAngle={Math.PI / 2.1} />
         </Canvas>
         <Configurator />
       </div>
